d7233f22ff82682137e27642c319e620c06134bd
lauraschoerken <lauraschorken@gmail.com>
2021-07-11 20:49:46 +0100

    nav, table, dashboard and product form. created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -27,18 +27,25 @@ const routes: Routes = [
         loadChildren: () =>
           import('./home/home.module').then((m) => m.HomeModule),
       },
-      {
-        path: '**',
-        loadChildren: () =>
-          import('./page-not-found/page-not-found.module').then((m) => m.PageNotFoundModule),
-      },
+
       {
         path: 'demo',
         loadChildren: () =>
           import('./demo/demo.module').then((m) => m.DemoModule),
       },
     ],
+  },  
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./admin/admin.module').then((m) => m.AdminModule),
   },
+  {
+    path: '**',
+    loadChildren: () =>
+      import('./page-not-found/page-not-found.module').then((m) => m.PageNotFoundModule),
+  },
+
 ];
 
 @NgModule({
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,13 +3,38 @@ import { CommonModule } from '@angular/common';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatBadgeModule} from '@angular/material/badge';
-import {MatCardModule} from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatButtonModule, MatToolbarModule,MatBadgeModule,MatCardModule, MatIconModule],
-  exports: [MatButtonModule, MatToolbarModule,MatBadgeModule,MatCardModule, MatIconModule],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatBadgeModule,
+    MatCardModule,
+    MatIconModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    ReactiveFormsModule,
+  ],
+  exports: [
+    MatButtonModule,
+    MatToolbarModule,
+    MatBadgeModule,
+    MatCardModule,
+    MatIconModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    ReactiveFormsModule,
+  ],
 })
 export class MaterialModule {}
